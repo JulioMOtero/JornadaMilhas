89a13e6b94ee14e352896bd30d3fc20c59da06e2
julio <julio@DESKTOP>
2024-07-18 16:01:40 -0300

    created new test for negative price

--- tests/JornadaMilhas.Teste/TripOfferTest.cs
+++ tests/JornadaMilhas.Teste/TripOfferTest.cs
@@ -32,5 +32,20 @@ namespace JornadaMilhas.Teste
             Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Errors.Sumario);
             Assert.False(oferta.IsValid);
         }
+
+        [Fact]
+        public void TestingNegativePrice()
+        {
+            //arrage
+            Route route = new Route("OriginTest", "DestinyTest");
+            Period period = new Period(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double price = -100.0;
+            //act
+
+            TripOffer offer = new TripOffer(route,period,price);
+            //assert
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.",offer.Errors.Sumario);
+
+        }
     }
 }
