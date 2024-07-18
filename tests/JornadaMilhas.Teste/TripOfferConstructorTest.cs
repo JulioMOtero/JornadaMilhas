15e4d625a516490daaef872c87def4552b9b49f8
julio <julio@DESKTOP>
2024-07-18 18:27:30 -0300

    added inline data for tests

--- tests/JornadaMilhas.Teste/TripOfferConstructorTest.cs
+++ tests/JornadaMilhas.Teste/TripOfferConstructorTest.cs
@@ -5,18 +5,23 @@ namespace JornadaMilhas.Teste
 {
     public class TripOfferConstructorTest
     {
-        [Fact]
-        public void ReturnsValidOfferWithValidData()
+        [Theory]
+        [InlineData("", null, "2024-01-01", "2024-01-02", 0, false)]
+        [InlineData("OriginTest", "DestinyTest", "2024-02-01", "2024-02-05", 100, true)]
+        [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+        [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+        [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+        public void ReturnsValidationAccordingToInputData(string origin, string destiny, string initialDate, string finalDate, double price, bool validation)
         {
             //arrange
-            Route route = new Route("OriginTest", "DestinyTest");
-            Period period = new Period(new DateTime(2024, 2, 1),new DateTime(2024, 2, 5));
-            double price = 100.0;
-            var validation = true;
+            Route route = new Route(origin, destiny);
+            Period period = new Period(DateTime.Parse(initialDate), DateTime.Parse(finalDate));
+
+
             //act
             TripOffer offer = new TripOffer(route, period, price);
             //assert
-            Assert.Equal(validation,offer.IsValid);
+            Assert.Equal(validation, offer.IsValid);
         }
         [Fact]
         public void ReturnsInvalidRouteOrPeriodErrorMessageWhenRouteIsNull()
@@ -34,19 +39,25 @@ namespace JornadaMilhas.Teste
             Assert.False(oferta.IsValid);
         }
 
-        [Fact]
-        public void ReturnsInvalidPriceErrorMessageWhenPriceIsLessThanZero()
+        [Theory]
+        [InlineData("OriginTest", "DestinyTest", "2024-01-01", "2024-01-02", 0, false)]
+        [InlineData("OriginTest", "DestinyTest", "2024-02-01", "2024-02-05", 100, true)]
+        [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+        [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+        [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+        public void ReturnsInvalidPriceErrorMessageWhenPriceIsLessThanZero(string origin, string destiny, string initialDate, string finalDate, double price, bool validation)
         {
             //arrage
-            Route route = new Route("OriginTest", "DestinyTest");
-            Period period = new Period(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
-            double price = -100.0;
+            Route route = new Route(origin, destiny);
+            Period period = new Period(DateTime.Parse(initialDate), DateTime.Parse(finalDate));
+
 
             //act
-            TripOffer offer = new TripOffer(route,period,price);
+            TripOffer offer = new TripOffer(route, period, price);
 
             //assert
-            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.",offer.Errors.Sumario);
+            Assert.Equal(validation, offer.IsValid);
+            //Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", offer.Errors.Sumario);
 
         }
     }
